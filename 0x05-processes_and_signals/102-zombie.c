f1007c5e9d1239c6ea5d7775ef79413005815cf8
VicvekSr <Olamideoso65@gmail.com>
2022-07-22 22:56:08 +0000

    corrrected betty errors

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -9,14 +9,13 @@
  *
  * Return: Always 0.
  */
-
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
@@ -24,22 +23,25 @@ int infinite_while(void)
  *
  * Return: Always 0.
  */
- int main(void)
- {
-    pid_t pid;
-    int i = 0;
+int main(void)
+{
+	pid_t pid;
+	char count = 0;
 
-    while (i < 5){
-        pid = fork();
-        if (pid > 0){
-            printf("Zombie process created, PID: %d\n", pid);
-            sleep(1);
-            i++;
-        }
-        else
-                exit(0);
-                }
-        infinite_while();
+	while (count < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			count++;
+		}
+		else
+			exit(0);
+	}
 
-        return 0;
+	infinite_while();
+
+	return (0);
 }
