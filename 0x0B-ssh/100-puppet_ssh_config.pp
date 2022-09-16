38b49f817ae0f42b04c7789c1b0685827b2d2f30
VicvekSr <Olamideoso65@gmail.com>
2022-09-16 18:17:16 +0000

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,5 @@
-# Client configuration file (w/ Puppet)
+# Set up client configuration file (w/ Puppet)
+
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
+}
