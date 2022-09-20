1796d9af70b16d925d316288e58ba91424585e38
VicvekSr <Olamideoso65@gmail.com>
2022-09-20 22:06:24 +0000

    puppet config file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,4 @@
 # Installs an Nginx server
-
 exec {'install':
   provider => shell,
   command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https://www.nginx.com/products/nginx-management-suite/ permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
