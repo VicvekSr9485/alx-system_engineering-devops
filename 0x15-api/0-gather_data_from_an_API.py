8ad61f06477531ccf1c686ad678f4b951729dc1b
VicvekSr <Olamideoso65@gmail.com>
2022-10-17 18:23:27 +0000

    add  a script that gathers data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+""" This module gathers data from an API """
+
+import json
+import requests
+import sys
+
+
+def get_user_todo_list():
+    employee_id = sys.argv[1]
+    url1 = 'https://jsonplaceholder.typicode.com/users/%s' % employee_id
+    url2 = '%s/todos' % url1
+    todo_list = requests.get(url2).json()
+    user = requests.get(url1).json()
+    completed_todo = []
+    for todo in todo_list:
+        if todo.get('completed') is True:
+            completed_todo.append(todo.get('title'))
+
+    print("Employee {} is done with tasks({}/{}): "
+          .format(user.get('name'), len(completed_todo), len(todo_list)))
+    for todo in completed_todo:
+        print("\t {}".format(todo))
+
+
+if __name__ == '__main__':
+    todo_list = get_user_todo_list()
