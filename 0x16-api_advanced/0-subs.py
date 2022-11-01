a44cf2735977b75678f68310c8fbc8a4b6dd2c5b
VicvekSr <Olamideoso65@gmail.com>
2022-11-01 19:20:05 +0000

    add a script that returns subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This module contains a function that queries
+    RedditAPI for subreddits """
+
+import requests
+import json
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """ Returns the number of subscribers for a given subreddit """
+    user_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': user_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    subs = response.json()
+    if 'data' not in subs:
+        return 0
+    if 'subscribers' not in subs.get('data'):
+        return 0
+    return response.json()['data']['subscribers']
