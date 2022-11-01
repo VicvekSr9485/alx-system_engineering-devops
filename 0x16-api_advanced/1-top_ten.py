0bc6392fcd07b8f0602429a070369606194bbc4b
VicvekSr <Olamideoso65@gmail.com>
2022-11-01 19:42:00 +0000

    returns top 10 hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" This module queries the Reddit API and prints the
+    titles of the first 10 hot posts
+        listed for a given subreddit
+"""
+
+import json
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """ Returns first 10 hot posts listed for
+        a given subreddit """
+    user_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': user_agent
+    }
+
+    params = {
+        'limit': 10
+    }
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
+
+    response = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    dic = response.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) is 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
