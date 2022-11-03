40eb336882aeb94fca55c9d5aa69c43dbadc6891
VicvekSr <Olamideoso65@gmail.com>
2022-11-03 04:26:23 +0000

    add a recurse function

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,14 +8,39 @@ import requests
 import sys
 
 
-def recurse(subreddit, hot_list=[]):
-    """ returns list containing the titles of
-        all hot articles for a given subreddit
-    """
-    user_agent = 'Mozilla/5.0'
+def add_title(hot_list, hot_posts):
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
 
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Queries to Reddit API """
+    user_agent = 'Mozilla/5.0'
     headers = {
         'User-Agent': user_agent
     }
 
-    
+    params = {
+        'after': after
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+
+    if res.status_code != 200:
+        return None
+
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(hot_list, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
