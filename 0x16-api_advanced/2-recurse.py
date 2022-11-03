32036fe3ab8b9018a2b7df6262b3dbccda07c0d0
VicvekSr <Olamideoso65@gmail.com>
2022-11-03 04:36:52 +0000

    add a recurse function

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,46 +1,26 @@
 #!/usr/bin/python3
-""" queries the Reddit API and returns a list
-    containing the titles of all hot articles
-        for a given subreddit """
-
-import json
+"""returns a list containing the titles of all hot articles
+    of all hot articles for a given subreddit"""
 import requests
-import sys
-
-
-def add_title(hot_list, hot_posts):
-    """ Adds item into a list """
-    if len(hot_posts) == 0:
-        return
-    hot_list.append(hot_posts[0]['data']['title'])
-    hot_posts.pop(0)
-    add_title(hot_list, hot_posts)
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """ Queries to Reddit API """
-    user_agent = 'Mozilla/5.0'
-    headers = {
-        'User-Agent': user_agent
-    }
-
-    params = {
-        'after': after
-    }
-
+    """recursive function that queries the Reddit API"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    params = {'after': after}
+    response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False)
 
-    if res.status_code != 200:
+    if response.status_code == 404:
         return None
+    for post in response.json().get("data").get("children"):
+        hot_list.append(post.get("data").get("title"))
 
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    add_title(hot_list, hot_posts)
-    after = dic['data']['after']
-    if not after:
-        return hot_list
-    return recurse(subreddit, hot_list=hot_list, after=after)
+    after = response.json().get("data").get("after")
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
