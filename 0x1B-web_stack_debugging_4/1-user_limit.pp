506dc83c062f1ee89017ff0312396ac6e9ade5a6
VicvekSr <Olamideoso65@gmail.com>
2022-11-13 04:38:47 +0000

    add os permissions

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,13 @@
 # Changes the OS configuration so that it is possible to login
 # with the holberton user and open a file without any error message
 
-exec {"substitute-1":
+exec {'substitute-1':
     provider => shell,
-    command => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-    before => Exec[substitute-2],
+    command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+    before   => Exec[substitute-2],
 }
 
-exec {"substitute-2":
+exec {'substitute-2':
     provider => shell,
-    command => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+    command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
